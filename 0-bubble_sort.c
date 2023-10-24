b5ed8feb6e267eb1bfd5058f7c5e9fd0503aafdb
MihdarAbbasher <mihdar.ph@gmail.com>
2023-10-24 05:35:44 -0700

    t0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort arr using bubble sort method
+ * @array: arr to be sorted
+ * @size: arr length
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp, swap;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swap = 0;
+		for (j = 0; j < size - 1; j++)
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				swap = 1;
+				print_array(array, size);
+			}
+		if (!swap)
+			break;
+	}
+}
+
